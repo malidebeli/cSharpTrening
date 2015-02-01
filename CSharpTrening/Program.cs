15db1f73f010ca35a651dca14d2ac6783c66c6c8
jstavalj <jstavalj@hotmail.com>
2015-02-01 19:13:17 +0100

    incijalna verzija

---
+++ CSharpTrening/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CSharpTrening
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vatrogasac v1 = new Vatrogasac { Ime = "Josip" };
+            Vatrogasac v2 = new Vatrogasac {Ime = "Mirko"};
+
+            VatrogasnoVozilo vzv1 = new VatrogasnoVozilo { vozac = v1 };
+            VatrogasnoVozilo vzv2 = new VatrogasnoVozilo {vozac = new Vatrogasac{Ime="Ana"}};
+
+            Tocka t = new Tocka ();
+            t.OsX = 100;
+            t.OsY = 200;
+
+
+
+            v2.Voziti(vzv1, t);
+            v1.Voziti(vzv1, t);
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharpTrening/Prosirivost_i_Polomorfizam.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTrening
+{
+    class Prosirivost_i_Polomorfizam
+    {
+    }
+
+    class Vatrogasac
+    {
+        public string Ime { get; set; }
+
+        public void GasitiVatru()
+        {
+            Console.WriteLine("{0} gasi vatru", Ime);
+        }
+
+        public void Voziti(VatrogasnoVozilo vozilo, Tocka koordinate)
+        {
+            if (vozilo.vozac != this)
+            {
+                Console.WriteLine("Gospon niste vozači nemrerete voziti vatrogasnog avtobusa :D!");
+                return;
+            }
+            
+            vozilo.Voziti(koordinate);
+        }    
+    
+    }
+
+
+    class VatrogasnoVozilo
+    {
+        public Vatrogasac vozac {get; set;}
+        public void Voziti(Tocka koordinate)
+        {   
+            if (vozac == null)
+	            {
+                    Console.WriteLine("Gospon niste vozači nemrerete voziti vatrogasnog avtobusa :D!");
+		            return;
+	            }
+            Console.WriteLine("Vozimo prema {0}, {1} točci", koordinate.OsX,koordinate.OsY);
+        }
+    }
+
+    class Tocka
+	{
+        public int OsX{get;set;}
+        public int OsY{get;set;}
+		
+	}
+
+
+}
